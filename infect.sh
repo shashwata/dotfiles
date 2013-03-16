92723fb1e230d3db2467379f71ccb012b9d1b0ef
shashwat <shashwat@flipkart.com>
2013-03-17 00:57:42 +0530

    infect now replaces bashrc if it exists

--- infect.sh
+++ infect.sh
@@ -10,6 +10,8 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt(){
 	rsync -v -e ssh --exclude ".git*" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "__old" --exclude ".bashrc" --exclude ".extra" --exclude "init" --exclude ".osx" -a . $USER@$host:~
+    ssh $USER@$host [ -e ~/bashrc ] && mv .bashrc .bashrc_old
+    rsync -v .bashrc $USER@$host:~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
