085872e2405e463a9638e1820418cec91f9f3c18
shashwat <shashwat@flipkart.com>
2013-03-17 16:14:17 +0530

    Updates to infect script that can sync changes to all servers or a given list of servers. Also added a way to not push if the git repo version has not changed

--- infect.sh
+++ infect.sh
@@ -1,26 +1,72 @@
-#!/bin/bash
+#!/bin/bash -e
+infected_host_db="$HOME/.infected"
+mkdir -p $infected_host_db
 
-host=$1
-if [ "x$host" == "x" ] ; then
-    echo "Please specify the host name to infect"
-    exit 1
+if [ "x$1" == "--force" -o "$1" == "-f" ] ; then
+    force=true
+    shift
+else
+    force=false
 fi
 
-cd "$(dirname "${BASH_SOURCE}")"
-git pull
 function doIt(){
+    host=$1
+    echo "Updating host: $host"
 	rsync -v -e ssh --exclude ".git*" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "__old" --exclude ".bashrc" --exclude ".extra" --exclude "init" --exclude ".osx" -a . $USER@$host:~
     ssh $USER@$host [ -e ~/bashrc ] && mv .bashrc .bashrc_old
     rsync -v .bashrc $USER@$host:~
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+function require_push() {
+    host=$1
+    host_db="$infected_host_db/$host"
+    touch "$host_db"
+    
+    if ! host $host > /dev/null ; then
+        echo "Invalid host: $host"
+        exit 2
+    fi
+
+    host_version=$(cat $host_db)
+    curr_version=$(git log -n 1 --format=%H )
+    
+    if [ "x$host_version" != "x$curr_version" ] ; then
+        doIt $host
+        echo $curr_version > "$host_db"
+    else
+        echo "$host is already at the latest"
+    fi
+}
+
+function pushToHost() {
+    if [ $force == true ] ; then
+        doIt $host
+    else
+        require_push $1
+    fi
+}
+
+if [ "x$1" == "x" ] ; then
+    echo "Please specify one or more host names to infect, '*' to sync all hosts under $infected_host_db"
+    exit 1
 fi
-unset doIt
+if [ "$1" == "*" ] ; then
+    hosts=($(ls $infected_host_db))
+else
+    hosts=($@)
+fi
+
+echo Updating following hosts: ${hosts[@]}
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 0
+fi
+
+cd "$(dirname "${BASH_SOURCE}")"
+git pull
+
+for i in "${hosts[@]}" ; do
+    pushToHost $i
+done
+
